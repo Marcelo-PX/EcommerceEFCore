16ff7d41cd4b045c708e1c39cffccab9f4176bb4
CHSFN <christoferferreira28@gmail.com>
2022-11-30 23:22:18 -0300

    listar produto completo

--- EcommerceFront/src/app/components/views/produto/listar-produto/listar-produto.component.ts
+++ EcommerceFront/src/app/components/views/produto/listar-produto/listar-produto.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Categoria } from 'src/app/models/categoria.models';
+import { Produto } from 'src/app/models/produto.models';
 
 @Component({
   selector: 'app-listar-produto',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-produto.component.css']
 })
 export class ListarProdutoComponent implements OnInit {
+  produtos?: Produto[];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.http.get<Produto[]>("https://localhost:5001/api/produto/listar")
+    //Executar a requisição
+    .subscribe({
+      next: (produtos) => {
+        this.produtos = produtos;
+        console.log(produtos)
+    }
+    })
   }
 
 }
